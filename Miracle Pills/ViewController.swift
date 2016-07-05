9c2c6fe202e21807e873930a7404f559381dbe13
memoeinc <admin@memoe.co>
2016-07-05 15:37:55 -0400

    Initial Commit

---
+++ Miracle Pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by Memoe on 2016-07-05.
+//  Copyright © 2016 Silvarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
