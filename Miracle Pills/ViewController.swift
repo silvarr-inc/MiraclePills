f0de0c09d4c4553897e18fb1f2c16a9faf804508
Julian Silva <JulianSilva@Julians-MacBook-Pro.local>
2016-07-07 15:23:50 -0400

    Miracle Pills App Final

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -2,14 +2,33 @@
 //  ViewController.swift
 //  Miracle Pills
 //
-//  Created by Memoe on 2016-07-05.
+//  Created by Silvarr on 2016-07-05.
 //  Copyright © 2016 Silvarr. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var miraclePillEmoji: UIImageView!
+    @IBOutlet weak var miraclePillsLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var streetAddressLabel: UILabel!
+    @IBOutlet weak var streetAddressTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var successImage: UIImageView!
+    @IBOutlet weak var buyNowButton: UIButton!
+    @IBOutlet weak var barDividerLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +38,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buyNowButtonPressed(_ sender: AnyObject) {
+        miraclePillEmoji.isHidden = true
+        miraclePillsLabel.isHidden = true
+        amountLabel.isHidden = true
+        fullNameLabel.isHidden = true
+        fullNameTextField.isHidden = true
+        streetAddressLabel.isHidden = true
+        streetAddressTextField.isHidden = true
+        cityLabel.isHidden = true
+        cityTextField.isHidden = true
+        stateLabel.isHidden = true
+        stateTextField.isHidden = true
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        zipCodeLabel.isHidden = true
+        zipCodeTextField.isHidden = true
+        successImage.isHidden = false
+        buyNowButton.isHidden = true
+        barDividerLabel.isHidden = true
+    }
 }
 
